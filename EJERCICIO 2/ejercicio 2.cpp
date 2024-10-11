78be7b06cf60e59e892947ee8263d874dcc0c9df
Sara Carola Riqueza Limachi <128417909+SaraRiqueza@users.noreply.github.com>
2024-10-10 20:55:48 -0400

    Add files via upload

---
+++ EJERCICIO 2/ejercicio 2.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int resta(int a, int b) {
+    return a - b;
+}
+
+int multiplicacion(int a, int b) {
+    return a * b;
+}
+
+float division(int a, int b) {
+    if (b != 0) {
+        return (float)a / b;
+    } else {
+        printf("No se puede realizar la división por cero.\n");
+        return 0;  // Retorno 0 en caso de división por cero
+    }
+}
+
+int main() {
+    int a, b;
+    
+    printf("Introduzca a: ");
+    scanf("%d", &a);
+    printf("Introduzca b: ");
+    scanf("%d", &b);
+    
+    printf("Suma: %d\n", suma(a, b));
+    printf("Resta: %d\n", resta(a, b));
+    printf("Multiplicación: %d\n", multiplicacion(a, b));
+    printf("División: %.2f\n", division(a, b));
+    
+    return 0;
+}
